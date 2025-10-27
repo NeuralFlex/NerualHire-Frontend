38a87c2bbea5429e11064710266f3019b083b6bd
Atiq Umer <atiqumer15@gmail.com>
2025-10-27 22:44:49 +0500

    Layout issue

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -5,26 +5,20 @@ import Header from "./Header";
 import { useSidebar } from "../components/SidebarContext";
 
 const Layout = () => {
-  const { 
-    isExpanded, 
-    isMobile, 
-    isMobileOpen 
-  } = useSidebar();
+  const { isExpanded } = useSidebar(); 
 
   const sidebarMarginClass = isExpanded ? "lg:ml-64" : "lg:ml-16";
 
   return (
     <div className="min-h-screen bg-gray-50">
       
-      {/* 1. Sidebar (Fixed) */}
       <Sidebar />
-
       <Header />
 
-      {/* 3. Main Content Wrapper: This is the area for the Header + Main Content */}
       <div
         className={`flex flex-col min-h-screen transition-all duration-300 ${sidebarMarginClass}`}
       >
+      {/* Main Content */}
         <main className="flex-grow pt-16 p-6"> 
           <Outlet />
         </main>
